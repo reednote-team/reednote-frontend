c1ea50050198b9aa2bbd7b467f642c1609f6f7ac
Ex10si0n <yzb.ex10si0n@icloud.com>
2021-12-13 22:52:53 +0800

    Create Vite Project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <Milkdown/>
+</template>
+
+<script>
+import Milkdown from "./components/Milkdown.vue";
+export default {
+  name: "App",
+  components: {
+    Milkdown,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Milkdown.vue
@@ -0,0 +1,39 @@
+<template>
+  <VueEditor :editor="editor" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Editor, rootCtx, defaultValueCtx } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { VueEditor, useEditor } from "@milkdown/vue";
+import { commonmark } from "@milkdown/preset-commonmark";
+
+
+export default defineComponent({
+  name: "Milkdown",
+  components: {
+    VueEditor,
+  },
+  setup: () => {
+    const editor = useEditor((root) =>
+        Editor.make()
+            .config((ctx) => {
+              ctx.set(rootCtx, root);
+              ctx.set(defaultValueCtx, "");
+            })
+            .use(nord)
+            .use(commonmark)
+    );
+    return {
+      editor,
+    };
+  },
+});
+</script>
+
+<style scoped>
+body {
+  background-color: white;
+}
+</style>
