d3e98f904200af4f00172fb75cd175a2b206945e
Kevyn <532775134@qq.com>
2022-01-13 12:52:46 +0800

    toc styling

--- src/components/Minimap.vue
+++ src/components/Minimap.vue
@@ -20,14 +20,14 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="fixed mt-16 w-64 hidden lg:block">
+  <div class="fixed mt-16 w-[18%] max-w-sm hidden lg:block">
     <div
       class="min-h-screen max-h-screen overflow-scroll scrollbar scrollbar-thin scrollbar-thumb-slate-700 pt-4 text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-900"
     >
       <a
         v-for="heading in headings"
         :href="`#${heading.hid}`"
-        class="block mx-2 py-1 hover:bg-gray-100 dark:hover:bg-gray-800"
+        class="block ml-2 mr-4 py-1 hover:bg-gray-100 dark:hover:bg-gray-800"
         :class="`text-gray-${heading.type > 4 ? 3 : heading.type}00`"
         :style="`padding-left: ${heading.type * 8}px; font-size: ${heading.type > 3 ? 12 : 4 + (6 - heading.type) * 3}px;`"
       >{{ heading.text }}</a>
